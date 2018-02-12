0515500444a940fe653aa2bc7ba584d5a87d82ef
Your Name <you@example.com>
2018-02-12 21:21:43 +0200

    Updates

--- src/settings.h
+++ src/settings.h
@@ -7,22 +7,8 @@
 #define MQTT_TOPIC_SET "set/garage/door"
 
 #define PIN_RELAY D1  // Relay for the Garage door
-#define PIN_SENSOR D2 // Garage Door MUK
+#define PIN_SENSOR D2 // Garage Door MUK PIN
 
 #define RELEY_IMPULSE_TIME 2000
-
-#define WIFI_SSID "****"
-#define WIFI_PASS "****"
-
-#define MQTT_SERVER "****"
-#define MQTT_SERVER_PORT 0000
-#define MQTT_SERVER_FINGERPRINT \
-  "** ** ** ** ** ** ** ** ** ** ** * * * ** ** * ** ** **"
-#define MQTT_USERNAME "****"
-#define MQTT_PASS "****"
-#define MQTT_PUBLISH_STATUS_INTERVAL 10000 // 10 sec
-
-#define ARDUINO_OTA_PASS_HASH "****"
-#define ARDUINO_OTA_PORT 0000
 
 #endif // ifndef SETTING_H
