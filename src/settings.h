79628f66927595584caf394ca07d547ce8d9025f
achuchev <achuchev@users.noreply.github.com>
2018-02-13 10:51:57 +0200

    Changes

--- src/settings.h
+++ src/settings.h
@@ -8,11 +8,12 @@
 
 #define PIN_RELAY D1         // Relay for the Garage door
 #define PIN_SENSOR_OPENED D2 // Garage Door MUK for opened door
-#define PIN_SENSOR_CLOSED D3 // Garage Door MUK for closed door
+#define PIN_SENSOR_CLOSED D5 // Garage Door MUK for closed door
 
 #define RELEY_IMPULSE_TIME 2000
 
 #define MQTT_PUBLISH_STATUS_INTERVAL 10000
-#define MQTT_PUBLISH_STATUS_REDUCED_INTERVAL 2000
+#define MQTT_PUBLISH_STATUS_REDUCED_INTERVAL 1000
 
 #endif // ifndef SETTING_H
+
